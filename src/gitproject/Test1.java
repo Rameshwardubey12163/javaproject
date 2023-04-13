0afa3225911b2ff2f4819d3f320b3e67c4e66533
Rameshwar dubey <RD@DESKTOP-430T6C4>
2023-04-13 18:25:06 +0530

    jhgyuftfty.....

--- src/gitproject/Test1.java
+++ src/gitproject/Test1.java
@@ -1,6 +1,17 @@
 package gitproject;
 
 import java.util.Scanner;
+ class Abc {
+	public void newClass() {
+	Scanner sc= new Scanner(System.in);
+	System.out.println("enter the value");
+	int e=sc.nextInt();
+    int f=sc.nextInt();
+    int g= e-f;
+    System.out.println(g);
+
+	}
+}
 
 public class Test1 {
 
@@ -10,7 +21,10 @@ public class Test1 {
 		int a=sc.nextInt();
 	    int b=sc.nextInt();
 	    int c=a+b;
+	    int d= a-b;
 	    System.out.println(c);
+Abc abj=new Abc();
+abj.newClass();
 
 	}
 
